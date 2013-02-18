32d12c9865fa4473a2db33943b0f8275e6639128
Andrew Kofink <ajkofink@gmail.com>
2013-02-18 18:13:14 -0500

    Segregating Ubuntu from OSX

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-ln -s ~/dots/.gitconfig_ubuntu ~/.gitconfig
-ln -s ~/dots/.zshrc ~/.zshrc
+current_file='.gitconfig'
+if [ -f "~/$current_file" ]; then
+	mv "~/$current_file" "$current_file.old"
+fi
+if [ ! -L "~/$current_file" ]; then
+	ln -s "~/dots/$current_file_ubuntu" "~/$current_file"
+fi
+
+current_file='.zshrc'
+if [ -f "~/$current_file" ]; then
+	mv "~/$current_file" "$current_file.old"
+fi
+if [ ! -L "~/$current_file" ]; then
+	ln -s "~/dots/$current_file_ubuntu" "~/$current_file"
+fi
