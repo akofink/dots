96441ef3aed74da2157a4a067fd43daab4b467b0
Andrew Kofink <ajkofink@gmail.com>
2013-02-18 19:05:03 -0500

    The ubuntu setup script seems to be complete and functional

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -9,17 +9,18 @@ fi
 
 for i in "${files[@]}"
 do
-	if [ -e "${HOME}/${i}" ]; then
-	echo "Moving ${HOME}/${i} to ${i}.old"
+	if [[ -e "${HOME}/${i}" && ! -L "${HOME}/${i}" ]]; then
+		echo "Moving ${HOME}/${i} to ${HOME}/${i}.old"
 		if [ ! $dry ]; then
 			mv "${HOME}/${i}" "${HOME}/${i}.old"
 		fi
+	elif [ -L "${HOME}/${i}" ]; then
+		echo "${i} is already set up!"
+		exit
 	fi
 
-	if [ ! -L "${HOME}/${i}" ]; then
-		echo "Creating symlink from ${HOME}/dots/${i}_ubuntu to ${HOME}/${i}"
-		if [ ! $dry ]; then
-			ln -s "${HOME}/dots/${i}_ubuntu" "${HOME}/${i}"
-		fi
+	echo "Creating symlink from ${HOME}/dots/${i}_ubuntu to ${HOME}/${i}"
+	if [ ! $dry ]; then
+		ln -s "${HOME}/dots/${i}_ubuntu" "${HOME}/${i}"
 	fi
 done
