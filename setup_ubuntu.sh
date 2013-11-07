675d071b412866c6eae7793d94ad7e6c86211744
Andrew Kofink <ajkofink@gmail.com>
2013-11-07 18:46:41 -0500

    Update from

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,26 +2,27 @@
 
 declare -a files=('.gitconfig' '.zshrc')
 platform="ubuntu"
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 if [[ $1 = "--dry-run" || $1 = "-d" ]]; then
-	echo "This is a dry run."
-	dry=1
+  echo "This is a dry run."
+  dry=1
 fi
 
 for i in "${files[@]}"
 do
-	if [[ -e "${HOME}/${i}" && ! -L "${HOME}/${i}" ]]; then
-		echo "Moving ${HOME}/${i} to ${HOME}/${i}.old"
-		if [ ! $dry ]; then
-			mv "${HOME}/${i}" "${HOME}/${i}.old"
-		fi
-	elif [ -L "${HOME}/${i}" ]; then
-		echo "${i} is already set up!"
-		continue
-	fi
+  if [[ -e "${HOME}/${i}" && ! -L "${HOME}/${i}" ]]; then
+    echo "Moving ${HOME}/${i} to ${HOME}/${i}.old"
+    if [ ! $dry ]; then
+      mv "${HOME}/${i}" "${HOME}/${i}.old"
+    fi
+  elif [ -L "${HOME}/${i}" ]; then
+    echo "${i} is already set up!"
+    continue
+  fi
 
-	echo "Creating symlink from ${HOME}/dots/${i}_${platform} to ${HOME}/${i}"
-	if [ ! $dry ]; then
-		ln -s "${HOME}/dots/${i}_${platform}" "${HOME}/${i}"
-	fi
+  echo "Creating symlink from ${HOME}/dots/${i}_${platform} to ${HOME}/${i}"
+  if [ ! $dry ]; then
+    ln -s "${HOME}/dots/${i}_${platform}" "${HOME}/${i}"
+  fi
 done
