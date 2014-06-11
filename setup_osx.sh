081b34f5988377b05785686db4b3eb5a842aff39
Andrew Kofink <ajkofink@gmail.com>
2014-06-11 09:56:07 -0400

    Update from

--- setup_osx.sh
+++ setup_osx.sh
@@ -4,21 +4,21 @@ declare -a files=('.env' '.gemrc' '.gitconfig' '.gitignore' 'NERDTreeBookmarks' 
 platform="osx"
 
 if [[ $1 = "--dry-run" || $1 = "-d" ]]; then
-	echo "This is a dry run."
-	dry=1
+  echo "This is a dry run."
+  dry=1
 fi
 
 for i in "${files[@]}"
 do
-	if [[ -e "${HOME}/${i}" ]]; then
-		echo "Moving ${HOME}/${i} to ${HOME}/${i}.old"
-		if [ ! $dry ]; then
-			mv "${HOME}/${i}" "${HOME}/${i}.old"
-		fi
-	fi
+  if [[ -e "${HOME}/${i}" ]]; then
+    echo "Moving ${HOME}/${i} to ${HOME}/${i}.old"
+    if [ ! $dry ]; then
+      mv "${HOME}/${i}" "${HOME}/${i}.old"
+    fi
+  fi
 
-	echo "Creating symlink from ${HOME}/dots/${i}_${platform} to ${HOME}/${i}"
-	if [ ! $dry ]; then
-		ln -s "${HOME}/dots/${i}_${platform}" "${HOME}/${i}"
-	fi
+  echo "Creating symlink from ${HOME}/dots/${i}_${platform} to ${HOME}/${i}"
+  if [ ! $dry ]; then
+    ln -s "${HOME}/dots/${i}_${platform}" "${HOME}/${i}"
+  fi
 done
