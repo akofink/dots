91944e41949c43357b29ac0dd14c2815c53917c9
Andrew Kofink <ajkofink@gmail.com>
2023-07-03 09:28:30 -0400

    Fix tmux; add some mux configs
    Signed-off-by: Andrew Kofink <ajkofink@gmail.com>

--- setup/tmux.sh
+++ setup/tmux.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-source "$DOTS_REPO/util.sh" # eval_template
+DEV_REPOS=${DEV_REPOS:-"$HOME/dev/repos"}
+DOTS_REPO="$DEV_REPOS/dots"
 
-eval_template "$DOTS_REPO/templates/.tmux.conf" "$HOME/.tmux.conf"
+TEMPLATE="$DOTS_REPO/templates/.tmux.conf"
+TMUX_CONF="$HOME/.tmux.conf"
+
+if [ ! -h $TMUX_CONF ]; then
+  if [ -f $TMUX_CONF ]; then
+    mv $TMUX_CONF{,.old.$(date +"%Y%m%dT%H%M%S")}
+  fi
+
+  ln -s $TEMPLATE $TMUX_CONF
+fi
