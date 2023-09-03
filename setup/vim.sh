eaef55ed64630e826ae67eb7efa39b09659303f5
Andrew Kofink <ajkofink@gmail.com>
2023-09-03 11:53:35 -0400

    Add vundle setup; sandbox mux config
    Signed-off-by: Andrew Kofink <ajkofink@gmail.com>

--- setup/vim.sh
+++ setup/vim.sh
@@ -2,7 +2,15 @@
 
 export DEV_REPOS=${DEV_REPOS:-"$HOME/dev/repos"}
 export DOTS_REPO="$DEV_REPOS/dots"
+export GITHUB_TOKEN=${GITHUB_TOKEN:-"''"}
 
 source "$DOTS_REPO/util.sh" # eval_template
 
 eval_template "$DOTS_REPO/templates/.vimrc" "$HOME/.vimrc"
+
+# Clone Vundle for package management
+mkdir -p ~/.vim/bundle
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
