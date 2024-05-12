1736303377399196cc8286b340de4ec6a8817fb3
Andrew Kofink <ajkofink@gmail.com>
2024-05-12 15:15:26 -0400

    fix rbenv
    Signed-off-by: Andrew Kofink <ajkofink@gmail.com>

--- setup/rbenv.sh
+++ setup/rbenv.sh
@@ -4,6 +4,8 @@ if [ ! -d ~/.rbenv ]; then
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 fi
 
+PATH=$PATH:~/.rbenv/bin
+
 if [ ! -d "$(rbenv root)"/plugins/ruby-build ]; then
   git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 fi
