1c0268f8ce9e2bc4ad82a8d70a555b7f0cc98e33
Andrew Kofink <ajkofink@gmail.com>
2024-11-10 22:29:45 -0500

    Install latest ruby with rbenv
    Signed-off-by: Andrew Kofink <ajkofink@gmail.com>

--- setup/rbenv.sh
+++ setup/rbenv.sh
@@ -9,3 +9,8 @@ PATH=$PATH:~/.rbenv/bin
 if [ ! -d "$(rbenv root)"/plugins/ruby-build ]; then
   git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 fi
+
+LATEST_RUBY_VERSION=$(rbenv install -l | grep -v - | tail -1)
+
+rbenv install $LATEST_RUBY_VERSION \
+  && rbenv global $LATEST_RUBY_VERSION
